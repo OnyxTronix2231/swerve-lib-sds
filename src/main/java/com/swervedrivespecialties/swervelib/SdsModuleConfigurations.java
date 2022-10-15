2257bc5cc89e885e6d30d5a965271a93a7e19f93
Alon Vaisgur <37458479+Romeore@users.noreply.github.com>
2022-10-15 19:12:06 +0300

    Changed MK3_FAST

--- src/main/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurations.java
+++ src/main/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurations.java
@@ -20,9 +20,9 @@ public final class SdsModuleConfigurations {
     
     public static final ModuleConfiguration MK3_FAST = new ModuleConfiguration(
             0.1016,
-            (16.0 / 48.0) * (28.0 / 16.0) * (15.0 / 60.0),
+            1/7.5,
             true,
-            (15.0 / 32.0) * (10.0 / 60.0),
+            1/12,
             true
     );
 
